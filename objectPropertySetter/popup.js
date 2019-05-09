572036ce23eda74a0d6f6cc125e7205e87a711f5
calmsz <limonggi@gmail.com>
2019-05-09 12:31:58 +0200

    ADDED VARIABLE OPTIONS: when pageContext is not working (console.log not logging) as supposed must validate that the current loaded tab matches the criteria from field contentScripts-matches in manifestJson. all good.

--- objectPropertySetter/popup.js
+++ objectPropertySetter/popup.js
@@ -1,16 +1,36 @@
 
-document.getElementById('qabutton').addEventListener('click', setServerUrl);
-document.getElementById('devbutton').addEventListener('click', setServerUrl);
+// loading user preferences
+
+chrome.storage.sync.get({
+  qaurl: 'http://cua.ultraviolento',
+  devurl: 'http://dev.ultraviolento'
+}, (items) => {
+  let urls = {
+    qaurl: '',
+    devurl: '',
+  }
+  urls.qaurl = items.qaurl;
+  urls.devurl = items.devurl;
+
+  var setServerUrl = function(element) {
+    let server = '';
+    server = (element.target.id === 'qabutton') ? urls.qaurl : urls.devurl;
+    
+    let params = {
+      server: server
+    }
+
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, params);
+      alert(params.server)
+    });
+  
+  }
+
+  document.getElementById('qabutton').addEventListener('click', setServerUrl);
+  document.getElementById('devbutton').addEventListener('click', setServerUrl);
+
+});
 
 
-function setServerUrl(element){
-  const params = {
-    server: 'http://dev.ultraviolento'
-  }
-  if (element.target.id === "qabutton") {
-    params.server = "http://cua.ultraviolento"
-  }
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, params);
-  });
-}
+
